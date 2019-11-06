c33d97a644c0d6a8cf52f56044657d73654f018f
Ivan Ravasi <ivan4.ravasi@gmail.com>
2019-11-06 15:16:25 +0100

    Add factorial
    Done with recursion. Because loops are boring.

---
+++ fact.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fact(int n) {
+    if (n==0) {
+        return 1;
+    } else {
+        return n*fact(n-1);
+    }
+}
+
+int main(void) {
+    printf("%d", fact(4));
+    return 0;
+}
