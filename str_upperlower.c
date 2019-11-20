377c7d0bd2e11880c790c7565d6491abe5a14665
Ivan Ravasi <ivan4.ravasi@gmail.com>
2019-11-20 14:55:40 +0100

    Add string exercise

---
+++ str_upperlower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define DIM 10
+
+int main(void) {
+    // Local variable declaration
+    char input[DIM];
+    // Input
+    scanf("%s", input);
+    // Processing
+    for (int i = 0; i < DIM; i++) {
+        if (input[i] >= 65 && input[i] <= 90) {
+            // UPPERCASE -> lowercase
+            input[i] += 32;
+        } else if (input[i] >= 97 && input[i] <= 122) {
+            // lowercase -> UPPERCASE
+            input[i] -= 32;
+        }
+        // Other chars. Do nothing
+    }
+    // Output
+    printf("%s", input);
+    return 0;
+}
