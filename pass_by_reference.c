c5c897f620c72ca1d9912ac43e25700700fe05d2
Ivan Ravasi <ivan4.ravasi@gmail.com>
2019-11-26 16:06:16 +0100

    Add pass by reference exercise

---
+++ pass_by_reference.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+void int_div(int *quotient, int *remainder, int a, int b)
+{
+    *quotient = a/b;
+    *remainder = a%b;
+}
+
+int main(void) {
+    int v[SIZE], n, q, r;
+    for (int i = 0; i < SIZE; i++) {
+        v[i] = rand() % 40 + 10;
+        printf("\t%d", v[i]);
+    }
+
+    printf("\nInserisci un numero: ");
+    scanf("%d", &n);
+    for (int i = 0; i < SIZE; i++) {
+        int_div(&q, &r, v[i], n);
+        printf("\t%d\t%d\t%d\t%d\n", v[i], n, q, r);
+    }    
+    
+    return 0;
+}
